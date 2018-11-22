2b6308c18b42f3a626aefa42fc90bf676eada40d
Sylvain <sylvain@pham.es>
2018-11-22 12:36:40 +0100

    dusk

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Dusk\DuskServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        if ($this->app->environment('local','testing')) {
+            # code...
+            $this->app->register(DuskServiceProvider::class);
+        }
     }
 }
